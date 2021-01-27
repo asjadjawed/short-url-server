c24b29fe57ab4c7b5e8c8413c33d32702fdfa935
Asjad Jawed Siddiqui Qadri <asjadjawed@gmail.com>
2021-01-27 21:36:13 +0500

    initial commit

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # Short URL - Server
 
 Backend server for URL shortening service.
+
+About the server:
+
+- URL shortening service
+- Graceful shutdown of server & database
+- Security via helmet
+- Decoupled backend (Node.js) and frontend (React)
+- Server code organized MVC style (Can also be organized component style)
+
+List of packages:
+
+- cors: for front-end / back-end connection
+- dotenv: to set environment variables for development.
+
+  ```.env
+  NODE_ENV=development
+  PORT=5500
+  ```
+
+- helmet: to secure against common attacks
---
+++ src/server.js
@@ -0,0 +1,63 @@
+import dotenv from "dotenv";
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import helmet from "helmet";
+
+dotenv.config(); // setting up env variables (check README.md)
+
+// setting up middleware
+const app = express();
+if (process.env.NODE_ENV === "development") app.use(morgan("tiny")); // setting up logger for dev env
+app.use(cors()); // to handle cors for decoupled fe-be connection
+app.use(helmet()); // security against common attack patterns
+app.use(express.json()); // we don't need body-parser package anymore (built-in with express)
+
+const server = app.listen(process.env.PORT, () =>
+  console.log(`Server running on port: ${process.env.PORT}`)
+);
+
+/*
+ * Handle graceful shutdown of server
+ * This is done manually here, 3rd party packages like terminus can be used
+ * https://www.npmjs.com/package/@godaddy/terminus
+ */
+
+// Connection logging - uncomment if needed
+// setInterval(
+//   () =>
+//     server.getConnections((err, connections) =>
+//       console.log(`${connections} connections currently open`)
+//     ),
+//   1000
+// );
+
+process.on("SIGTERM", shutDown);
+process.on("SIGINT", shutDown);
+
+let connections = [];
+
+server.on("connection", (connection) => {
+  connections.push(connection);
+  connection.on(
+    "close",
+    () => (connections = connections.filter((curr) => curr !== connection))
+  );
+});
+
+function shutDown() {
+  console.log("Received kill signal, shutting down gracefully");
+  server.close(() => {
+    console.log("Closed out remaining connections");
+    process.exit(0);
+  });
+
+  setTimeout(() => {
+    console.error(
+      "Could not close connections in time, forcefully shutting down"
+    );
+    process.exit(1);
+  }, 10000);
+  connections.forEach((curr) => curr.end());
+  setTimeout(() => connections.forEach((curr) => curr.destroy()), 5000);
+}
