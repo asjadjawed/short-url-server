c824dfb679182ebf0e2a798c80fe85e4d9f84dad
Asjad Jawed Siddiqui Qadri <asjadjawed@gmail.com>
2021-01-28 01:50:19 +0500

    updated connection for atlas

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,21 +1,10 @@
 import mongoose from "mongoose";
-mongoose.set("useNewUrlParser", true);
-mongoose.set("useFindAndModify", false);
-mongoose.set("useCreateIndex", true);
-mongoose.set("useUnifiedTopology", true);
-mongoose.promise = global.Promise;
 
 export const connectDB = async () => {
-  const { DB_ADDRESS, DB_NAME, DB_USER, DB_PASS } = process.env;
-  const url = `${DB_ADDRESS}${DB_NAME}`;
-
-  return await mongoose.connect(url, {
-    auth: {
-      authSource: "admin",
-    },
-    user: DB_USER,
-    pass: DB_PASS,
-  });
+  const { DB_CONNECTION } = process.env;
+  console.log(DB_CONNECTION);
+  const url = DB_CONNECTION;
+  return await mongoose.connect(url);
 };
 
 export const disconnectDB = async () => {
