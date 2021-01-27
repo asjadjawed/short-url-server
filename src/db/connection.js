cce61647d54b2a2948bcd40f76736b717ee1ea56
Asjad Jawed Siddiqui Qadri <asjadjawed@gmail.com>
2021-01-28 02:01:21 +0500

    removing debug log

--- src/db/connection.js
+++ src/db/connection.js
@@ -2,7 +2,6 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   const { DB_CONNECTION } = process.env;
-  console.log(DB_CONNECTION);
   const url = DB_CONNECTION;
   return await mongoose.connect(url, {
     useNewUrlParser: true,
