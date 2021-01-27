9725856c3cf3c9f9377b3e813a0bf247d2c5c662
Asjad Jawed Siddiqui Qadri <asjadjawed@gmail.com>
2021-01-28 01:57:59 +0500

    connection settings

--- src/db/connection.js
+++ src/db/connection.js
@@ -4,7 +4,10 @@ export const connectDB = async () => {
   const { DB_CONNECTION } = process.env;
   console.log(DB_CONNECTION);
   const url = DB_CONNECTION;
-  return await mongoose.connect(url);
+  return await mongoose.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 };
 
 export const disconnectDB = async () => {
